41ece352c097d39f34d9f68e860d32b845d58d6b
Desmends27 <desmondopokumends2004@gmail.com>
2024-06-30 18:52:35 +0000

    documented modules and functions

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
+""" Pascal triangle generation """
+
+
 def factorial(n):
+    """ Generate factorial"""
     fact = 1
     for num in range(2, n + 1):
         fact *= num
     return fact
 
+
 def combination(n, r):
+    """ Use combinations formula to get values"""
     return int((factorial(n)) /
                ((factorial(r)) * factorial(n - r)))
 
 
 def pascal_triangle(rows):
+    """ Append values to list """
     result = []
     for count in range(rows):
         row = []
