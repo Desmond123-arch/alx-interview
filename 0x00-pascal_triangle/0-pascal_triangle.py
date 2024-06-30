71b002710f6a0f973184b2b0321c0645b6ecff77
Desmends27 <desmondopokumends2004@gmail.com>
2024-06-30 18:50:39 +0000

    removed factorial

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-import math
-
+def factorial(n):
+    fact = 1
+    for num in range(2, n + 1):
+        fact *= num
+    return fact
 
 def combination(n, r):
-    return int((math.factorial(n)) /
-               ((math.factorial(r)) * math.factorial(n - r)))
+    return int((factorial(n)) /
+               ((factorial(r)) * factorial(n - r)))
 
 
 def pascal_triangle(rows):
