5dcf64a25395f7eb7254fdd27dc1819a16c53dcb
Desmends27 <desmondopokumends2004@gmail.com>
2024-07-19 00:00:09 +0000

    format error

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-""" 
-Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size> (if the format is not this one, the line must be skipped)
-After every 10 lines and/or a keyboard interruption (CTRL + C), print these statistics from the beginning:
+"""
+Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+<status code> <file size>
+ (if the format is not this one, the line must be skipped)
+After every 10 lines and/or a keyboard interruption
+(CTRL + C), print these statistics from the beginning:
 Total file size: File size: <total size>
-where <total size> is the sum of all previous <file size> (see input format above)
+where <total size> is the sum of all previous
+ <file size> (see input format above)
 Number of lines by status code:
-possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
-if a status code doesn’t appear or is not an integer, don’t print anything for this status code
+possible status code: 200, 301, 400, 401, 403,
+ 404, 405 and 500
+if a status code doesn’t appear or is not an integer,
+don’t print anything for this status code
 format: <status code>: <number>
 status codes should be printed in ascending order
 """
@@ -27,7 +33,14 @@ count = 1
 status_dicts = {}
 while True:
     log_line = read_line()
-    pattern = r'(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(?P<date>[^\]]+)\] "GET /projects/260 HTTP/1.1" (?P<status>\d{3}) (?P<size>\d+)'
+    pattern = (
+        r'(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - '
+        r'\[(?P<date>[^\]]+)\] '
+        r'"GET /projects/260 HTTP/1.1" '
+        r'(?P<status>\d{3}) '
+        r'(?P<size>\d+)'
+    )
+
     match = re.match(pattern, log_line)
     if match:
         ip = match.group('status')
