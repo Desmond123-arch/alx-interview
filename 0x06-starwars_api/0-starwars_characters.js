03b86df93322bf9079bc4f9f2747606dd7072cd9
Desmends27 <desmondmends27@gmail.com>
2024-08-10 02:09:34 +0000

    tes

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,35 +2,45 @@
 'use strict';
 
 const request = require('request');
-
-require('process');
+const process = require('process');
 
 function getPerson (url) {
   request(url, (error, response, body) => {
-    if (error) console.log(error);
+    if (error) {
+      console.error(`Error fetching person from ${url}:`, error);
+      return;
+    }
     try {
       const data = JSON.parse(body);
       const person = data.name;
       console.log(person);
     } catch (e) {
-      console.log(e);
+      console.error(`Error parsing JSON from ${url}:`, e);
     }
   });
 }
 
-let people;
 if (process.argv.length > 2) {
   const id = process.argv[2];
-  const url = 'https://swapi-api.alx-tools.com/api/films/' + id + '/';
+  const url = `https://swapi-api.alx-tools.com/api/films/${id}/`;
+
   request(url, (error, response, body) => {
-    if (error) console.log(error);
+    if (error) {
+      console.error(`Error fetching film data from ${url}:`, error);
+      return;
+    }
 
     try {
       const data = JSON.parse(body);
-      people = data.characters;
-      people.map(getPerson);
+      const people = data.characters;
+
+      if (people && Array.isArray(people)) {
+        people.forEach(getPerson);
+      } else {
+        console.error('Invalid data format for characters:', people);
+      }
     } catch (e) {
-      console.log(e);
+      console.error('Error parsing JSON from film data:', e);
     }
   });
 }
